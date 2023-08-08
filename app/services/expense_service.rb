5b0cd45a2b4e4127698575e1119cd0001fed223d
rounak-yubi <rounak.bhowmick@go-yubi.com>
2023-08-08 19:55:09 +0530

    added thirt party api to validate invoice

--- app/services/expense_service.rb
+++ app/services/expense_service.rb
@@ -1,5 +1,7 @@
 # app/services/expense_service.rb
 class ExpenseService
+  API_URL = 'https://my.api.mockaroo.com/invoices.json'.freeze
+  API_KEY = 'b490bb80'.freeze
   def initialize(expense_params)
     @expense_params = expense_params
   end
@@ -11,6 +13,25 @@ class ExpenseService
       { success: true, expense: expense }
     else
       { success: false, errors: expense.errors.full_messages }
+    end
+  end
+
+  def create_invoice_with_third_party_api(invoice_number)
+    puts "Correct: #{invoice_number}"
+    uri = URI(API_URL)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Post.new(uri.path)
+    request['X-API-Key'] = API_KEY
+    request.body = { invoice_id: invoice_number.to_i }.to_json
+    puts "request: #{JSON.parse(request.body)}"
+    response = http.request(request)
+    puts "response: #{JSON.parse(response.body)}"
+    if response.code.to_i == 200
+      { success: true, response: JSON.parse(response.body) }
+    else
+      { success: false, error: "API request failed: #{response.message}" }
     end
   end
 end
