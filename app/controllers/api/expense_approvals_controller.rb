113a2d3494ce525ab48f8cda2341a23b45fb1d73
rounak-yubi <rounak.bhowmick@go-yubi.com>
2023-08-08 14:39:37 +0530

    updated expense controller

--- app/controllers/api/expense_approvals_controller.rb
+++ app/controllers/api/expense_approvals_controller.rb
@@ -1,19 +1,28 @@
 class Api::ExpenseApprovalsController < ApplicationController
-  #   before_action :authenticate_user!
+  # before_action :authenticate_user!
 
-  def create
-    expense_approval = ExpenseApproval.new(expense_approval_params)
-    puts "Outputting expense details: #{expense_approval_params}"
-    if expense_approval.save
-      render json: { message: 'Expense approval successful!', expense_approval: expense_approval }, status: :created
+  def update
+    expense = Expense.find_by(id: params[:id])
+    employee = Employee.find_by(id: expense_approval_params[:approved_by_id])
+
+    if expense && employee && employee.role == 'admin'
+      expense_approval = ExpenseApproval.find_or_initialize_by(expense_id: expense.id)
+
+      if expense_approval.update(expense_approval_params)
+        render json: { message: 'Expense approval successful!', expense_approval: expense_approval }, status: :created
+      else
+        render json: { errors: expense_approval.errors.full_messages }, status: :unprocessable_entity
+      end
+    elsif !employee || employee.role != 'admin'
+      render json: { message: 'Access denied' }, status: :forbidden
     else
-      render json: { errors: expense_approval.errors.full_messages }, status: :unprocessable_entity
+      render json: { message: 'Expense not found' }, status: :not_found
     end
   end
 
   private
 
   def expense_approval_params
-    params.require(:expense_approval).permit(:expense_id, :approved_by_id, :status, :comments)
+    params.require(:expense_approval).permit(:approved_by_id, :status, :comments)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :employees, only: [:create]
     resources :expenses, only: [:create]
-    resources :expense_approvals, only: [:create]
+    resources :expense_approvals, only: %i[create update]
     get 'expenses/:user_id', to: 'expenses#index_by_user', as: :expenses_by_user
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
